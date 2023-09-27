26b37ab70bf90fc9091b49f756f68c1fae9427b5
Tanja Varvio <tanja.varvio@koud.fi>
2023-09-27 10:19:01 +0300

    Create array containing persons data

---
+++ index.js
@@ -0,0 +1,27 @@
+let persons = [
+	{
+		"id": 1,
+		"name": "Arto Hellas",
+		"number": "040-123456"
+	},
+	{
+		"id": 2,
+		"name": "Ada Lovelace",
+		"number": "453-4651325"
+	},
+	{
+		"id": 3,
+		"name": "Dan Abramov",
+		"number": "032-52352351"
+	},
+	{
+		"id": 4,
+		"name": "Mary Poppendick",
+		"number": "342-235231"
+	},
+	{
+		"id": 5,
+		"name": "Amy Winehouse",
+		"number": "945-4252345"
+	}
+]
